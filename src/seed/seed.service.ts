e5b65c73166f1f4373c5f600929ac02969dee1a4
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-05-02 20:20:00 -0300

    feat: .

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -21,43 +21,72 @@ export class SeedService {
   ) {}
 
   async seed() {
-    const seriesId = 344;
+    const SERIES_ID = 344;
 
     const [characters, comics, creators] = await Promise.all([
       fetch(
-        `${MARVEL_API_URL}/series/${seriesId}/characters?ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
+        `${MARVEL_API_URL}/series/${SERIES_ID}/characters?ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
       ).then((res) => res.json()),
       fetch(
-        `${MARVEL_API_URL}/series/${seriesId}/comics?ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
+        `${MARVEL_API_URL}/series/${SERIES_ID}/comics?ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
       ).then((res) => res.json()),
       fetch(
-        `${MARVEL_API_URL}/series/${seriesId}?ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
+        `${MARVEL_API_URL}/series/${SERIES_ID}?ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
       ).then((res) => res.json()),
     ]);
 
-    const res = await Promise.all([
-      characters.data.results.forEach((char: any) => {
-        this.characterService.create({
-          name: char.name,
-          description: char.description,
-          thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
-        });
-      }),
-      comics.data.results.forEach((comic: any) => {
-        this.comicService.create({
-          title: comic.title,
-          description: comic.description,
-          thumbnail: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
-        });
-      }),
-      creators.data.results[0].creators.items.forEach((creator: any) => {
-        this.creatorService.create({
+    const charactersIds = characters.data.results.map((char: any) => char.id);
+
+    const createdCharacters = characters.data.results.map((char: any) => {
+      return this.characterService.create({
+        name: char.name,
+        description: char.description,
+        thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      });
+    });
+
+    const createdComics = comics.data.results.map((comic: any) => {
+      return this.comicService.create({
+        title: comic.title,
+        description: comic.description ?? comic.title,
+        thumbnail: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
+      });
+    });
+
+    const createdCreators = creators.data.results[0].creators.items.map(
+      (creator: any) => {
+        return this.creatorService.create({
           name: creator.name,
           role: creator.role.toUpperCase(),
         });
-      }),
+      }
+    );
+
+    const [charactersResult, comicsResult, creatorsResult] = await Promise.all([
+      Promise.all(createdCharacters),
+      Promise.all(createdComics),
+      Promise.all(createdCreators),
     ]);
 
-    return res;
+    for (let x = 0; x < charactersResult.length; x++) {
+      charactersResult[x].id = charactersIds[x];
+    }
+
+    console.log(
+      `${MARVEL_API_URL}/series/${SERIES_ID}/comics?ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
+    );
+    console.log(charactersResult);
+
+    const teste = await fetch(
+      `${MARVEL_API_URL}/comics?characters=avengers&ts=${MARVEL_TS}&apikey=${MARVEL_API_KEY}&hash=${MARVEL_HASH}`
+    ).then((res) => res.json());
+
+    console.log(teste);
+
+    return {
+      characters: charactersResult,
+      comics: comicsResult,
+      creators: creatorsResult,
+    };
   }
 }
