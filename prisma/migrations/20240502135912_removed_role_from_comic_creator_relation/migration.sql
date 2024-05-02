6021bd950de3a39b135ad481a5f9fc9d53e66bd4
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-05-02 11:12:16 -0300

    chore: removed role from comic creator relation crud

---
+++ prisma/migrations/20240502135912_removed_role_from_comic_creator_relation/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `ComicCreator` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ComicCreator" DROP COLUMN "role";
