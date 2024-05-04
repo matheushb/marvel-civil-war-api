f22dfc4d7ca9565f16a190e126cd051909ffce89
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-05-04 18:50:56 -0300

    feat: added cluster

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import app from "./app";
+import cluster from "cluster";
+import os from "os";
 import { PORT } from "./common/constants";
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/api`);
+  if (cluster.isPrimary) {
+    console.log(`Swagger running on http://localhost:${PORT}/api`);
+  }
 });
