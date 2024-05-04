ea9159af854f80ec266d5fa7cea1769df8ba141f
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-05-04 18:53:27 -0300

    lol

--- src/user/dto/user-query.params.ts
+++
@@ -1,9 +0,0 @@
-enum userIdFilterType {
-  TASK_QUANTITY = "TASK_QUANTITY",
-  OLDEST_TASK = "OLDEST_TASK",
-  MOST_RECENT_TASK = "MOST_RECENT_TASK",
-}
-
-export interface QueryParams {
-  userIdFilterType: userIdFilterType;
-}
